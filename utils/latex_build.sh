f30cdd74049d1d9c82d1512250db903635112104
Leo <lstwn@mailbox.org>
2024-12-09 18:17:59 +0100

    add thesis template

--- utils/latex_build.sh
+++ utils/latex_build.sh
@@ -4,14 +4,20 @@ arg=$1
 # without extension
 file=$2
 row=$3
+source_dir=$(dirname $file)
+build_dir="$source_dir/build"
+
+echo $source_dir $build_dir
 
 function build() {
   /usr/local/bin/tectonic -X compile $file.tex \
-    --untrusted --synctex --keep-logs --keep-intermediates
+    --untrusted --synctex --keep-logs --keep-intermediates \
+    --outdir "$build_dir"
 }
 
 function forward_search() {
-  /Applications/Skim.app/Contents/SharedSupport/displayline -g -r $row $file.pdf
+  /Applications/Skim.app/Contents/SharedSupport/displayline \
+    -g -r "$row" "$build_dir/$file.pdf"
 }
 
 function clean() {
